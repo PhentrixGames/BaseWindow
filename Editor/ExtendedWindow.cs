5300b77da67ec955319711b0abcefdd7d85072ea
Dorky106 <greasontim@gmail.com>
2021-02-02 14:52:13 -0500

    Add files via upload

--- Editor/ExtendedWindow.cs
+++ Editor/ExtendedWindow.cs
@@ -1,16 +1,48 @@
+using System.IO;
 using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
 
-public abstract class ExtendedWindow<T>: EditorWindow where T: EditorWindow
+public abstract class ExtendedWindow<T, U>: EditorWindow where T: EditorWindow where U: ScriptableObject
 {
-    VisualElement root;
+    protected T wnd;
+    protected U selectedItem;
+    protected VisualElement root;
+
     public static void Init(string name)
     {
-        T wnd = GetWindow<T>();
+        wnd = GetWindow<T>();
         wnd.titleContent = new GUIContent(name);
     }
 
     public void CreateGUI()
     {
         root = rootVisualElement;
+    }
+
+    public void PopulateList()
+    {
+        VisualElement ItemList = root.Q<VisualElement>("ItemList");
+        string[] items = AssetDatabase.FindAssets("t:" + typeof(U).Name);
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            string path = AssetDatabase.GUIToAssetPath(items[i]);
+            Button btn = new Button();
+            btn.text = Path.GetFileNameWithoutExtension(path);
+            btn.clickable.clicked += () =>
+            {
+                selectedItem = AssetDatabase.LoadAssetAtPath<U>(path);
+            };
+        }
+    }
+
+    protected abstract void UpdateItemInfo();
+
+    public void Save()
+    {
+        EditorUtility.SetDirty(selectedItem);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
     }
 }
