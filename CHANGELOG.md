f5fed22bb45351fc960e44c3fc8121b3f2d68c66
Dorky106 <greasontim@gmail.com>
2021-02-03 10:02:13 -0500

    Add files via upload

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 0.2.0
-* Added README, LICENSE, CHANGELOG
-* Added some documention
+** Added
+* README, LICENSE, CHANGELOG
+* Some documention
+** Fixes
+* Issue where ExtendedWindow couldn't be found
 
 0.1.0
 * Initial release
--- Editor/ExtendedWindow.cs
+++ Editor/ExtendedWindow.cs
@@ -3,53 +3,56 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public abstract class ExtendedWindow<T, U>: EditorWindow where T: EditorWindow where U: ScriptableObject
+namespace PhentrixGames.Editor
 {
-    protected T wnd;
-    protected U selectedItem;
-    protected VisualElement root;
-
-    public static void Init(string name)
+    public abstract class ExtendedWindow<T, U> : EditorWindow where T : EditorWindow where U : ScriptableObject
     {
-        wnd = GetWindow<T>();
-        wnd.titleContent = new GUIContent(name);
-    }
+        protected T wnd;
+        protected U selectedItem;
+        protected VisualElement root;
 
-    public void CreateGUI()
-    {
-        root = rootVisualElement;
-    }
-
-    /// <summary>
-    /// Populates an ItemList visual element with buttons for each scriptableobject of type U
-    /// </summary>
-    /// <param name="itemList">Name of ItemList visual element</param>
-    public void PopulateList(string itemList)
-    {
-        VisualElement ItemList = root.Q<VisualElement>(itemList);
-        string[] items = AssetDatabase.FindAssets("t:" + typeof(U).Name);
-
-        for (int i = 0; i < items.Length; i++)
+        public static void Init(string name)
         {
-            string path = AssetDatabase.GUIToAssetPath(items[i]);
-            Button btn = new Button();
-            btn.text = Path.GetFileNameWithoutExtension(path);
-            btn.clickable.clicked += () =>
-            {
-                selectedItem = AssetDatabase.LoadAssetAtPath<U>(path);
-            };
+            wnd = GetWindow<T>();
+            wnd.titleContent = new GUIContent(name);
         }
-    }
 
-    protected abstract void UpdateItemInfo();
+        public void CreateGUI()
+        {
+            root = rootVisualElement;
+        }
 
-    /// <summary>
-    /// Saves and Refreshs AssetDatabase
-    /// </summary>
-    public void Save()
-    {
-        EditorUtility.SetDirty(selectedItem);
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
+        /// <summary>
+        /// Populates an ItemList visual element with buttons for each scriptableobject of type U
+        /// </summary>
+        /// <param name="itemList">Name of ItemList visual element</param>
+        public void PopulateList(string itemList)
+        {
+            VisualElement ItemList = root.Q<VisualElement>(itemList);
+            string[] items = AssetDatabase.FindAssets("t:" + typeof(U).Name);
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                string path = AssetDatabase.GUIToAssetPath(items[i]);
+                Button btn = new Button();
+                btn.text = Path.GetFileNameWithoutExtension(path);
+                btn.clickable.clicked += () =>
+                {
+                    selectedItem = AssetDatabase.LoadAssetAtPath<U>(path);
+                };
+            }
+        }
+
+        protected abstract void UpdateItemInfo();
+
+        /// <summary>
+        /// Saves and Refreshs AssetDatabase
+        /// </summary>
+        public void Save()
+        {
+            EditorUtility.SetDirty(selectedItem);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
     }
 }
